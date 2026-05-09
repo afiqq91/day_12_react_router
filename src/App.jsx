451f87d3e8d0b42f7d21019c948abacc51fc6b6e
afiqq91 <muhammadafiqq91@gmail.com>
2026-05-09 15:55:52 +0800

    complete excercise 1 basic routing setup

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import InstructorListPage from "./pages/InstructorListPage";
+import InstructorDetailPage from "./pages/InstructorDetailPage";
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+
+                <Route path="/" element={<HomePage />} />
+
+                <Route
+                    path="/instructors"
+                    element={<InstructorListPage />}
+                />
+
+                <Route
+                    path="/instructors/:id"
+                    element={<InstructorDetailPage />}
+                />
+
+                <Route
+                    path="/login"
+                    element={<LoginPage />}
+                />
+
+                <Route
+                    path="/dashboard"
+                    element={<DashboardPage />}
+                />
+
+                <Route
+                    path="*"
+                    element={<NotFoundPage />}
+                />
+
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
---
+++ src/pages/InstructorListPage.jsx
@@ -0,0 +1,8 @@
+export default function InstructorListPage() {
+    return (
+        <div>
+            <h1>Instructor List Page</h1>
+            <p>List of all instructors.</p>
+        </div>
+    );
+}
